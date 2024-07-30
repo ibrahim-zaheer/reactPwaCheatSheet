33053fa26818ec2590a2ef7fa7dc5aeca1f7751b
ibrahim-zaheer <zaheeribrahim346@gmail.com>
2024-07-31 00:19:42 +0500

    notifcation half done by include webpack.config.js and config-overrides.js

---
+++ config-overrides.js
@@ -0,0 +1,16 @@
+// config-overrides.js
+const webpack = require('webpack');
+
+module.exports = function override(config, env) {
+  config.resolve.fallback = {
+    ...config.resolve.fallback,
+    buffer: require.resolve('buffer/'),
+  };
+  config.plugins = [
+    ...config.plugins,
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+  ];
+  return config;
+};
--- src/swDev.js
+++ src/swDev.js
@@ -1,6 +1,32 @@
-export default function swDev(){
-    let swUrl = `${process.env.PUBLIC_URL}/sw.js`
-    navigator.serviceWorker.register(swUrl).then((response)=>{
-        console.warn("response",response)
-    })
-}
+export default function swDev() {
+    function urlBase64ToUint8Array(base64String) {
+      const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+      const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
+      const rawData = window.atob(base64);
+      const outputArray = new Uint8Array(rawData.length);
+      for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+      }
+      return outputArray;
+    }
+  
+    function determineAppServerKey() {
+      const validPublicKey = "BH0WBXA6HLq8kxSxB3Wmt5PTexCipR15lsTJSL2ja3-r8Ea48XUFNHXK-NyFbpxWmYleORk1w24l4-K4boLyGBY";
+      return urlBase64ToUint8Array(validPublicKey);
+    }
+  
+    let swUrl = `${process.env.PUBLIC_URL}/sw.js`;
+  
+    navigator.serviceWorker.register(swUrl).then((response) => {
+      console.warn("response", response);
+      response.pushManager.getSubscription().then((subscription) => {
+        if (!subscription) {
+          response.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: determineAppServerKey(),
+          });
+        }
+      });
+    });
+  }
+  
